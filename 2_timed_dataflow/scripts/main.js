389106da92f8e114223f79e620f40b5b7b59e5a8
sishirsubedi <subedisishir@gmail.com>
2018-02-12 23:05:35 -0600

    completed timed flow plot

--- 2_timed_dataflow/scripts/main.js
+++ 2_timed_dataflow/scripts/main.js
@@ -2,7 +2,6 @@
 ;(function() {
 
   console.log("starting page...")
-  	//var margin = { top: 10, right: 10, bottom: 10, left: 10 };
 
   var data;
   var dataXRange = { min: 0, max: 100 };
@@ -10,18 +9,19 @@
    
    var width = 800;
    var height = 500;
-   var circleRadius =10;
+   var circleRadius =7;
+
 
    var canvas = d3.select("body")
    				.append("svg")
    				.attr("width", width)
    				.attr("height",height+100)
    				.append("g")
-   				.attr("transform","translate(50,0)")
+   				.attr("transform","translate(0,0)")
 
    var widthscalex = d3.scaleLinear()
    				    .domain([dataXRange.min, dataXRange.max])
-   				    .range([50,width])
+   				    .range([0,width])
    
    var widthscaley = d3.scaleLinear()
    				    .domain([dataYRange.min, dataYRange.max])
@@ -29,7 +29,7 @@
 
 
    var xaxis = d3.axisBottom()
-              .ticks(10)
+              .ticks(20)
               .scale(widthscalex)
 
 
@@ -46,9 +46,9 @@
    		  .attr("transform","translate(50,0)")
    		  .call(yaxis)
 
-   d3.json("./data/stream_1b.json", function(error, json) {
+   d3.json("./data/stream_1.json", function(error, json) {
 			if (error) {
-				return console.warn(error);
+				return console.warn("error",error);
 			} else {
 				data = json;
 			}
@@ -57,7 +57,7 @@
 
 
 
-function updatecircles() {
+function updatecircles(tick) {
 
   var theCircles = canvas.selectAll('.roundy')
         .data(data, function (d, i) {return d.id })
@@ -69,62 +69,91 @@ function updatecircles() {
         .attr('cx', function (d, i) { return widthscalex(d.xVal) } )
         .attr('cy', function (d, i) { return widthscaley(d.yVal) } )
         .style('fill', 'steelblue')
-        //.merge(theCircles)
-         .on("mouseover", function(d) {
-         	 for (var i = 1; i < 5; ++i) {
-		            var position = d3.mouse(canvas.node());
+        .on("mouseover", function(d) {
+            d3.selectAll('.roundy')
+  					.filter(function(d) {
 
-		            var circle = canvas.append("circle")
-		                    .attr("cx", position[0])
-		                    .attr("cy", position[1])
-		                    .attr("r", 0)
-		                    .style("stroke-width", 5 / (i))
-		                    .transition()
-		                        .delay(Math.pow(i, 2.5) * 50)
-		                        .duration(2000)
-		                        .ease('quad-in')
-		                    .attr("r", r)
-		                    .style("stroke-opacity", 0)
-		                    .each("end", function () {
-		                        d3.select(this).remove();
-		                    }); 
-		                    }})                 
-		  .on("mouseout", function(d) {
-  					
+  						      if( (widthscalex(d.xVal) < d3.event.clientX+75) && (widthscalex(d.xVal) > d3.event.clientX-75) &&
+  						          (widthscaley(d.yVal) < d3.event.clientY+75) && (widthscaley(d.yVal) > d3.event.clientY-75)){
+  						       	return d
 
-  					 })
-         .transition()
+  						      }
+  					                      })
+         	        .transition()
+         	        .style('fill', 'yellow')
+         	        .attr("r", circleRadius + 5)
+         	        .transition()
+         	        	.style('fill', 'red')
+         	        	.attr("r", circleRadius + 5)
+         	        	.transition()
+         	        	.attr("r", circleRadius)
+         	        	.style('fill', 'steelblue')
+
+         	   // .filter(function(d2) {
+
+  						     //  if( (widthscalex(d2.xVal) > d3.event.clientX+50) && (widthscalex(d2.xVal) < d3.event.clientX+75) &&
+  						     //      (widthscaley(d2.yVal) > d3.event.clientY+50) && (widthscaley(d2.yVal) < d3.event.clientY+75) &&
+  						     //      (widthscalex(d2.xVal) < d3.event.clientX-50) && (widthscalex(d2.xVal) > d3.event.clientX-75) &&
+  						     //      (widthscaley(d2.yVal) < d3.event.clientY-50) && (widthscaley(d2.yVal) > d3.event.clientY-75)
+
+  						     //  ){
+  						     //   	return d2
+
+  						     //  }
+  					      //                 })
+         	   //          .transition()
+         	   //      	.style('fill', 'pink')
+         	   //      	.attr("r", circleRadius + 50)
+	         	  //       	.transition()
+	         	  //       	.attr("r", circleRadius)
+	         	  //       	.style('fill', 'steelblue')
+         	        
+         	       
+         	       
+
+		                    })                 
+	    .on("mouseout", function(d) {
+            d3.selectAll('.roundy')
+  					//.filter(function(d2) { return d2.xVal == d.xVal;})
+         	        //.transition()
+         	        .style('fill', 'steelblue')
+         	        .attr("r", circleRadius)
+         	       // .ease("elastic-out")
+
+		                    })  
+
+
+
+
+// move the dots 
+         theCircles.transition()
           	.delay(0)
-          	.duration(20000)
-           .attr('cx', function (d, i) { return widthscalex(1) } )
-           .attr('cy', function (d, i) { return widthscaley(d.yVal)} )
-          .style('fill', 'steelblue')
-           .transition()
-            .delay(1)
-           .duration(10)
-           .attr('cx', function (d, i) { 
-            if (d.xVal ==1){
-            	d3.select(".roundy").remove()
-            }
-           } )
-           .delay(1)
-           .duration(10)
-           .style("fill",'white')
+          	.duration(1000)
+           .attr('cx', function (d) { 
+
+           	d.xVal = d.xVal- 1
+           return widthscalex(d.xVal) })
+           
+// remove dots when near y axis
+         d3.selectAll('.roundy')
+  					.filter(function(d2) { return d2.xVal<8;})
+                    .remove()
+
 }
 
 
 function updateaxes(tick) {
 
    	d3.select(".xaxisb").remove();
-
-    var widthscalex = d3.scaleLinear()
-   				    .domain([dataXRange.min, dataXRange.max])
-   				    .range([0-10*tick,width])
    
-
+    var widthscalex2 = d3.scaleLinear()
+   				    .domain([dataXRange.min, dataXRange.max])
+   				    .range([-(12*tick),width])
+   
+   console.log("tick is ", tick)
    var xaxis = d3.axisBottom()
-              .ticks(10)
-              .scale(widthscalex)
+              .ticks(20)
+              .scale(widthscalex2)
 
    canvas.append("g")
    		  .attr("transform","translate(50,500)")
@@ -133,10 +162,13 @@ function updateaxes(tick) {
     
 
         }
-var btn = d3.select("body");
-var menuButton = btn.append("button")
+
+var timer = d3.timer(timerCallback); 
+
+var restart_btn = d3.select("#chartDiv")
+    .append("button")
     .text("Restart")
-    .attr("id", "buttonCentre")
+    .attr("position", "right")
     .classed("button", true)
     .on('click', function(){
         console.log("restart")
@@ -144,6 +176,23 @@ var menuButton = btn.append("button")
 
     });
 
+var pause_btn = d3.select("#chartDiv")
+    .append("button")
+    .text("Pause")
+    .classed("button", true)
+    .on('click', function(){
+        console.log("pause")
+        timer.stop()
+    });
+
+var play_btn = d3.select("#chartDiv")
+    .append("button")
+    .text("Play")
+    .classed("button", true)
+    .on('click', function(){
+        console.log("play")
+        timer.restart(timerCallback); 
+    });
 
 
 //
@@ -155,24 +204,23 @@ function timerCallback(elapsed) {
     tick = tick + 1;
 
     
-    var newThing = {
-      id: tick,
-      xVal: data[0].xVal,
-      yVal: data[0].yVal,
-    }
-    data.push(newThing);
+    // var newThing = {
+    //   id: tick,
+    //   xVal: data[0].xVal,
+    //   yVal: data[0].yVal,
+    // }
+    // data.push(newThing);
 
-    //remove from data?
-    data.shift();
-    //console.log(jsonData)
+    // data.shift();
+    // console.log(jsonData)
 
-    updatecircles();
+    updatecircles(tick);
     updateaxes(tick);
 
     timer.restart(timerCallback);
   }
 }
 
-var timer = d3.timer(timerCallback);
+
 
 	})();
